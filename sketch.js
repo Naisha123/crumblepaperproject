6b55112266a58e942d18740ec1121dc6c8838ada
Naisha123 <69965468+Naisha123@users.noreply.github.com>
2020-09-19 16:10:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinobj,paperobj,groundobj;
+var world
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+dustbinobj = new Dustbin(1200,650);
+paperobj = new Paper(200,450,40);
+groundobj = new Ground(width/2,670,width,20);
+var render = Render.create({
+	 element: document.body,
+	  engine: engine,
+	   options: {
+			width: 1200,
+			 height: 700, 
+			 wireframes: false
+			 } });
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  dustbinobj.display();
+  paperobj.display();
+  groundobj.display();
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85})
+	}
+}
+
+
